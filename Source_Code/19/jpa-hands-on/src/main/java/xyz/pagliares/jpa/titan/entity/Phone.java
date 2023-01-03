67350908ef00282d3268ada97664cf883bd2528f
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2023-01-03 20:14:19 -0300

    Create many-to-many bidirectional mappings

---
+++ Source_Code/19/jpa-hands-on/src/main/java/xyz/pagliares/jpa/titan/entity/Phone.java
@@ -0,0 +1,56 @@
+package xyz.pagliares.jpa.titan.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Phone implements Serializable {
+    private int id;
+    private String number;
+    private int type;
+    public Phone() {
+    }
+
+    public Phone(String number, int type) {
+        this.number = number;
+        this.type = type;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                ", type=" + type +
+                '}';
+    }
+}
---
+++ Source_Code/19/jpa-hands-on/src/main/java/xyz/pagliares/jpa/titan/entity/Reservation.java
@@ -0,0 +1,73 @@
+package xyz.pagliares.jpa.titan.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Reservation {
+    private int id;
+    private float amountPaid;
+    private LocalDateTime date;
+    private Cruise cruise;
+    private Set<Customer> customers = new HashSet<>();
+
+    public Reservation() {
+    }
+
+    public Reservation(Cruise cruise) {
+        this.cruise = cruise;
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="AMOUNT_PAID")
+    public float getAmountPaid() {
+        return amountPaid;
+    }
+
+    public void setAmountPaid(float amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+
+    @Column(name="DATE_RESERVED")
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @ManyToOne
+    @JoinColumn(name="CRUISE_ID")
+    public Cruise getCruise() {
+        return cruise;
+    }
+
+    public void setCruise(Cruise cruise) {
+        this.cruise = cruise;
+    }
+
+    @ManyToMany
+    @JoinTable(name="RESERVATION_CUSTOMER",
+                joinColumns={@JoinColumn(name="RESERVATION_ID")},
+                inverseJoinColumns={@JoinColumn(name="CUSTOMER_ID")})
+    public Set<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Set<Customer> customers) {
+        this.customers = customers;
+    }
+}
