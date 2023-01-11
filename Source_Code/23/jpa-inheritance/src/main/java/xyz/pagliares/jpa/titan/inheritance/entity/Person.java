52562d19c3a63375c20a6aaf550cdcd454f33550
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2023-01-11 14:28:09 -0300

    Create inheritance (table per subclass - join) example

---
+++ Source_Code/23/jpa-inheritance/src/main/java/xyz/pagliares/jpa/titan/inheritance/entity/Person.java
@@ -0,0 +1,37 @@
+package xyz.pagliares.jpa.titan.inheritance.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
