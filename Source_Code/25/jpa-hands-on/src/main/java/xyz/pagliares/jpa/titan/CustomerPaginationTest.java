1e9aaf58392d178cd20372450b36a2a4e96cfc18
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2023-01-12 14:43:39 -0300

    Demonstrate how to get all customers in batches aiming performance improvement

--- Source_Code/25/jpa-hands-on/src/main/java/xyz/pagliares/jpa/titan/CustomerPaginationTest.java
+++ Source_Code/25/jpa-hands-on/src/main/java/xyz/pagliares/jpa/titan/CustomerPaginationTest.java
@@ -4,6 +4,7 @@ import xyz.pagliares.jpa.titan.entity.Customer;
 import xyz.pagliares.jpa.titan.integration.CustomerDAO;
 import xyz.pagliares.jpa.titan.utility.DatabaseUtility;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class CustomerPaginationTest {
@@ -11,8 +12,33 @@ public class CustomerPaginationTest {
         DatabaseUtility.populateDatabase();
         CustomerDAO customerDAO = DatabaseUtility.getCustomerDAO();
         List<Customer> customers = customerDAO.getCustomers(3,5);
+
+        System.out.println(""" 
+                                        
+                                        List 3 customers, starting with ID 6
+        """);
+
+
         for (Customer customer:customers) {
             System.out.println(customer);
         }
+
+        System.out.println("""
+                            
+                            List all customers, in small steps aiming performance improvement
+        """);
+        List results;
+        int first = 0;
+        int max =3;
+        do {
+            results = customerDAO.getCustomers(max, first);
+            Iterator<Customer> it = results.iterator();
+            while(it.hasNext()){
+                System.out.println(it.next());
+            }
+            customerDAO.getEntityManager().clear();
+            System.out.println("---------------------");
+            first = first + results.size();
+        } while (results.size() > 0);
     }
 }
